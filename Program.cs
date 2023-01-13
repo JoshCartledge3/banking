277151a461cf60f6a1254a421d8941d9b536c92c
JoshCartledge3 <106667950+JoshCartledge3@users.noreply.github.com>
2023-01-13 09:49:23 +0000

    Created empty classes for Account parent and 3 account children (Personal, ISA, Business)

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿async static void Loading(int time)
+{
+    for (int i = 0; i < time; i++)
+    {
+        await Task.Delay(300);
+        Console.Write(".");
+    }
+}
+
+Console.ForegroundColor = ConsoleColor.Black;
+Console.BackgroundColor= ConsoleColor.Green;
+Console.Write("Loading");
+Loading(5);
+await Task.Delay(2000);
+Console.WriteLine();
+bool continueRunning = true;
+
+while (continueRunning)
+{
+
+
+
+
+
+
+
+
+    Console.Write("Continue? Y/N: ");
+    if (Console.ReadLine() == "n") continueRunning = false;
+}
