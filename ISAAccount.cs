a5b3d7f7ad660ffa3b8331b22db73e298fade702
JoshCartledge3 <106667950+JoshCartledge3@users.noreply.github.com>
2023-01-13 11:59:06 +0000

    Class and interface setup

--- ISAAccount.cs
+++ ISAAccount.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BankingMainApplication
 {
-    internal class ISAAccount
+    public class ISAAccount : Account
     {
     }
 }
--- PersonalAccount.cs
+++ PersonalAccount.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BankingMainApplication
 {
-    internal class PersonalAccount
+    public class PersonalAccount : Account
     {
     }
 }
