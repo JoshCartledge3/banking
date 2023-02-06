5ac62375b680516af6116373a2855c4b3fc4d99d
JoshCartledge3 <106667950+JoshCartledge3@users.noreply.github.com>
2023-02-06 16:40:18 +0000

    Created initial menu and added groundwork for verification class.

--- Program.cs
+++ Program.cs
@@ -8,13 +8,30 @@ namespace BankingMainApplication
         {
             Console.ForegroundColor = ConsoleColor.Green;
 
+            // Display main menu options
+            Console.WriteLine("This is a banking application. Please select a service?");
+            Console.WriteLine("Transactions. (1)");
+            Console.WriteLine("Accounts. (2)");
+            Console.WriteLine("Validation. (3)");
+            // Read result
+            string? menuDialogResult = Console.ReadLine();
 
-            Console.WriteLine("This is a banking application.");
-
-            TransactionService.GetTransactionDetails();
-            AccountService.GetAccountDetails();
-            ValidationService.GetValidationDetails();
+            // Return requested data
+            switch (menuDialogResult)
+            {
+                case "1":
+                    TransactionService.GetTransactionDetails();
+                    return;
+                case "2":
+                    AccountService.GetAccountDetails();
+                    return;
+                case "3":
+                    ValidationService.GetValidationDetails();
+                    ValidationService.VerifyPersonalAccount();
+                    return;
+                default:
+                    break;
+            }
         }
-
     }
 }
--- Services/ValidationServices.cs
+++ Services/ValidationServices.cs
@@ -6,5 +6,69 @@
         {
             Console.WriteLine("GetValidationDetails");
         }
+
+        #region Account Verification
+
+        /// <summary>
+        /// The three basic checks we perform on every account verification
+        /// </summary>
+        /// <returns>Returns true if all checks are successful</returns>
+        private static bool BasicVerification()
+        {
+            bool verificationResult;
+            string[] basicCheckResults = new string[3];
+
+            /// Perform checks
+            Console.WriteLine("Does the applicant have a valid photo ID? [Y/N]");
+            basicCheckResults[0] = Console.ReadLine().ToLower();
+            Console.WriteLine("Does the applicant have a valid address based ID? [Y/N]");
+            basicCheckResults[1] = Console.ReadLine().ToLower();
+            Console.WriteLine("Has the applicant provided the minimum deposit fee of £1.00 [Y/N]");
+            basicCheckResults[2] = Console.ReadLine().ToLower();
+
+            // Compare the results with a successful result
+            verificationResult = basicCheckResults.SequenceEqual(new string[] {"y", "y", "y"});
+            return verificationResult;
+        }
+
+        /// <summary>
+        /// Verification process for a customer who is attempting to open a personal account
+        /// </summary>
+        /// <returns>True if the account is verified, else false</returns>
+        public static bool VerifyPersonalAccount()
+        {
+            bool accountVerified = false;
+            Console.WriteLine("Personal account verification");
+
+            BasicVerification();
+            return accountVerified;
+        }
+
+        /// <summary>
+        /// Verification process for a customer who is attempting to open a ISA account
+        /// </summary>
+        /// <returns>True if the account is verified, else false</returns>
+        public static bool VerifyISAAccount()
+        {
+            bool accountVerified = false;
+            Console.WriteLine("ISA account verification");
+
+            BasicVerification();
+            return accountVerified;
+        }
+
+        /// <summary>
+        /// Verification process for a customer who is attempting to open a business account
+        /// </summary>
+        /// <returns>True if the account is verified, else false</returns>
+        public static bool VerifyBusinessAccount()
+        {
+            bool accountVerified = false;
+            Console.WriteLine("Business account verification");
+
+            BasicVerification();
+            return accountVerified;
+        }
+        #endregion
     }
 }
