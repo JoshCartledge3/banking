5cbd712a08e719f1c87c373458b0b61a7b4fc25e
JoshCartledge3 <106667950+JoshCartledge3@users.noreply.github.com>
2023-02-10 10:52:11 +0000

    Created "View Account" Method

--- Account.cs
+++ Account.cs
@@ -14,6 +14,31 @@ namespace BankingMainApplication
         public string? Forename { get; set; }
         public string? Surname { get; set; }
         public DateOnly DateOfBirth { get; set; }
-    }
 
+        #region Account Methods
+
+        /// <summary>
+        /// Displays a customer's account details
+        /// </summary>
+        /// <param name="accountID"></param>
+        /// <param name="accountType"></param>
+        /// <returns>String array of a customer's account detials</returns>
+        public static string[] ViewAccount(string accountID, string accountType)
+        {
+            try
+            {
+                foreach (var account in File.ReadAllLines(accountType + ".csv"))
+                {
+                    if (account.Contains(accountID)) return account.Trim().Split(',');
+                }
+                return new string[] { "No account found." };
+            }
+            catch
+            {
+                return new string[] { "Database could not be reached." };
+            }
+        }
+
+        #endregion
+    }
 }
